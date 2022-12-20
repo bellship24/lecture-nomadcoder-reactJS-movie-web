2aee85ff468613e22b0c51d8294cb319536ae861
bellship24 <bellship24@gmail.com>
2022-12-20 23:33:26 +0900

    7.3 Movie app part one

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -17,17 +17,6 @@ function App() {
     getMovies();
   }, []);
 
-  // useEffect(() => {
-  //   fetch(
-  //     'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
-  //   )
-  //     .then((response) => response.json())
-  //     .then((json) => {
-  //       setMovies(json.data.movies);
-  //       setLoading(false);
-  //     });
-  // }, []);
-
   console.log(movies);
   return (
     <div>
@@ -36,7 +25,16 @@ function App() {
       ) : (
         <div>
           {movies.map(movie => (
-            <div key={movie.id}>{movie.title}</div>
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
         </div>
       )}
